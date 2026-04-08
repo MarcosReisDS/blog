047df3fd1059e18c2d2fe1c297e0cbcaa29427cb
Marcos Reis <marcos.santos@estadao.com>
2026-04-08 01:10:15 -0300

    aula 104

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+    return (
+        <>
+            <h1>About</h1>
+            <a href="http://localhost:3000/" style={{ background: "red" }}>Ir para home</a>
+        </>
+    )
+}
