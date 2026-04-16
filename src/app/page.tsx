d9ac46f8646ffbdd985eee609aed58be5174450a
Marcos Reis <marcos.santos@estadao.com>
2026-04-15 23:38:47 -0300

    aula 112

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import { Header } from "@/components/Header";
+import { SpinLoader } from "@/components/SpinLoader";
 
 export default function HomePage() {
     console.log("HomePage rendered");
     return (
         <div>
-            <Header />
+            <SpinLoader />
         </div>
     )
 }
