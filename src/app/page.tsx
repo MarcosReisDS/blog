18776eafeb5d646fe52617c19442081b2b6efca1
Marcos Reis <marcos.santos@estadao.com>
2026-04-28 21:00:12 -0300

    aula 114

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
-import { Header } from "@/components/Header";
 import { PostsList } from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
-import { postRepository } from "@/repositories/post/json-post-repository";
 import { Suspense } from "react";
 
 export default async function HomePage() {
 
     return (
-        <div>
+        <div className="text-slate-900 bg-slate-100 min-h-screen">
             <header>
                 <h1 className="text-6xl font-bold text-center py-8">Aqui é header</h1>
             </header>
