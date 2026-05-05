b3d4a7b1290a617e03fdca21d600fcdd889b7500
Marcos Reis <marcos.santos@estadao.com>
2026-05-04 21:15:06 -0300

    aula 123

---
+++ src/utils/format-datetime.ts
@@ -0,0 +1,17 @@
+import { format, formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
+
+export function formatDateTime(rawDate: string): string {
+    const date = new Date(rawDate);
+    return format(date, "dd/MM/yyyy 'às' HH'h'mm", {
+        locale: ptBR
+    })
+}
+
+export function formatRelative(rawDate: string): string {
+    const date = new Date(rawDate);
+    return formatDistanceToNow(date, {
+        locale: ptBR,
+        addSuffix: true
+    })
+}
