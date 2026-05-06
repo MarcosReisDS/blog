3dbd2ba827763646b3eb599a95685812e82d8aa3
Marcos Reis <marcos.santos@estadao.com>
2026-05-05 21:15:54 -0300

    aula 126

--- src/components/PostFeatured/index.tsx
+++ src/components/PostFeatured/index.tsx
@@ -1,10 +1,12 @@
 import { PostCoverImage } from "../PostCoverImage";
-import { PostHeading } from "../PostHeading";
 import { PostSummary } from "../PostSummary";
+import { findAllPublicPosts } from "@/lib/post/queries";
 
-export function PostFeatured() {
-    const slug = "titulo-do-post";
-    const postLink = `/post/${slug}`;
+export async function PostFeatured() {
+    const posts = await findAllPublicPosts();
+    const post = posts[0];
+
+    const postLink = `/post/${post.slug}`;
 
     return (
         <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
@@ -15,8 +17,8 @@ export function PostFeatured() {
                 imageProps={{
                     width: 1200,
                     height: 720,
-                    src: "/images/bryen_9.png",
-                    alt: "Titulo do post",
+                    src: post.coverImageUrl,
+                    alt: post.title,
                     priority: true
                 }}
             />
@@ -24,9 +26,9 @@ export function PostFeatured() {
             <PostSummary
                 postHeading="h1"
                 postLink={postLink}
-                createdAt={"2025-04-08T00:24:38.616Z"}
-                title={"Rotina matinal de pessoas altamente eficazes"}
-                excerpt={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio ducimus esse et necessitatibus maxime consequatur aperiam voluptas quaerat quis. Sunt quisquam nulla in doloribus atque! Totam sequi pariatur laborum placeat!"}
+                createdAt={post.createdAt}
+                title={post.title}
+                excerpt={post.excerpt}
             />
         </section>
     )
