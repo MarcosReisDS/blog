cda8e8099a2148966f955e8062d42f61fd577f41
Marcos Reis <marcos.santos@estadao.com>
2026-05-07 20:27:56 -0300

    aula 133

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,12 +1,22 @@
 import { findPostBySlugCached } from "@/lib/post/queries"
+import { Metadata } from "next"
 
 type PostSlugProps = {
     params: Promise<{ slug: string }>
 }
 
+export async function generateMetadata({ params }: PostSlugProps): Promise<Metadata> {
+    const { slug } = await params
+    const post = await findPostBySlugCached(slug)
+
+    return {
+        title: post.title,
+        description: post.excerpt
+    }
+}
+
 export default async function PostSlugPage({ params }: PostSlugProps) {
     const { slug } = await params
-
     const post = await findPostBySlugCached(slug)
 
     return (
