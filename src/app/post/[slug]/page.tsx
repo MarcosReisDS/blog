a7a5ec2479c6dd35e34f24b53cdc9e1cded836b2
Marcos Reis <marcos.santos@estadao.com>
2026-05-06 21:30:36 -0300

    aula 131

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,10 +1,26 @@
+import { findPostBySlugCached } from "@/lib/post/queries"
+import { notFound } from "next/navigation";
+
 type PostSlugProps = {
     params: Promise<{ slug: string }>
 }
 
 export default async function PostSlugPage({ params }: PostSlugProps) {
     const { slug } = await params
+
+    let post;
+    
+    try {
+        post = await findPostBySlugCached(slug)
+    } catch {
+        post = undefined
+    }
+
+    if(!post) notFound()
+
     return (
-        <h1 className="text-7xl font-extrabold py-16">{slug}</h1>
+        <div>
+            <p>{post.title}</p>
+        </div>
     )
 }
--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -38,6 +38,17 @@ export class JsonPostRepository implements PostRepository {
 
         return post;
     }
+
+    async findBySlug(slug: string): Promise<PostModel> {
+        await this.simulateWait();
+
+        const posts = await this.findAllPublic();
+        const post = posts.find((post) => post.slug === slug);
+
+        if (!post) throw new Error('Post não encontrado');
+
+        return post;
+    }
 }
 
 export const postRepository: PostRepository = new JsonPostRepository();
