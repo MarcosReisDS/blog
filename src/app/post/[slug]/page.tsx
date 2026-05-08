3a366b5b098c39341756ed3575340b8e4359edd4
Marcos Reis <marcos.santos@estadao.com>
2026-05-07 21:01:34 -0300

    aula 134

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,5 +1,8 @@
+import { SinglePost } from "@/components/SinglePost"
+import { SpinLoader } from "@/components/SpinLoader"
 import { findPostBySlugCached } from "@/lib/post/queries"
 import { Metadata } from "next"
+import { Suspense } from "react"
 
 type PostSlugProps = {
     params: Promise<{ slug: string }>
@@ -17,11 +20,10 @@ export async function generateMetadata({ params }: PostSlugProps): Promise<Metad
 
 export default async function PostSlugPage({ params }: PostSlugProps) {
     const { slug } = await params
-    const post = await findPostBySlugCached(slug)
 
     return (
-        <div>
-            <p>{post.title}</p>
-        </div>
+        <Suspense fallback={<SpinLoader className="min-h-20 mb-16" />}>
+            <SinglePost slug={slug} />
+        </Suspense>
     )
 }
--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -5,7 +5,7 @@ import { readFile } from "fs/promises"
 
 const ROOT_DIR = process.cwd();
 const JSON_FILE_PATH = resolve(ROOT_DIR, "src", "db", "seed", "posts.json");
-const SIMULATE_WAIT_IN_MS = 0;
+const SIMULATE_WAIT_IN_MS = 5000;
 
 export class JsonPostRepository implements PostRepository {
     private async simulateWait() {
@@ -29,8 +29,6 @@ export class JsonPostRepository implements PostRepository {
     }
 
     async findById(id: string): Promise<PostModel> {
-        await this.simulateWait();
-
         const posts = await this.findAllPublic();
         const post = posts.find((post) => post.id === id);
 
@@ -40,8 +38,6 @@ export class JsonPostRepository implements PostRepository {
     }
 
     async findBySlug(slug: string): Promise<PostModel> {
-        await this.simulateWait();
-
         const posts = await this.findAllPublic();
         const post = posts.find((post) => post.slug === slug);
 
