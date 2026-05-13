d50cb4dbb20fa7970cfe7c8b7b1229ad59f83db6
Marcos Reis <marcos.santos@estadao.com>
2026-05-13 20:13:16 -0300

    aula 145

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import { SinglePost } from "@/components/SinglePost"
 import { SpinLoader } from "@/components/SpinLoader"
-import { findAllPublicPostsCached, findPostBySlugCached } from "@/lib/post/queries"
+import { findPostBySlugCached } from "@/lib/post/queries"
 import { Metadata } from "next"
 import { Suspense } from "react"
 
@@ -16,16 +16,6 @@ export async function generateMetadata({ params }: PostSlugProps): Promise<Metad
         title: post.title,
         description: post.excerpt
     }
-}
-
-export async function generateStaticParams() {
-    const posts = await findAllPublicPostsCached();
-
-    return posts.map(post => {
-        return {
-            slug: post.slug
-        }
-    })
 }
 
 export default async function PostSlugPage({ params }: PostSlugProps) {
