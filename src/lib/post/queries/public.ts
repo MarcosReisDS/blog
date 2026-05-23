443cd2bb6db0b1cbff2d69579c9d5ea8be97c50e
Marcos Reis <marcos.santos@estadao.com>
2026-05-23 14:24:20 -0300

    aula 164

--- src/lib/post/queries/public.ts
+++ src/lib/post/queries/public.ts
@@ -3,21 +3,27 @@ import { unstable_cache } from "next/cache";
 import { notFound } from "next/navigation";
 import { cache } from "react";
 
-export const findAllPublicPostsCached = unstable_cache(
-    cache(async () => await postRepository.findAllPublic()),
-    ['posts'],
-    {
-        tags: ['posts'],
-    })
+export const findAllPublicPostsCached = cache(
+    unstable_cache(
+        async () => await postRepository.findAllPublic(),
+        ['posts'],
+        {
+            tags: ['posts'],
+        })
+)
 
-export const findPublicPostBySlugCached = (slug: string) => unstable_cache(cache(async (slug: string) => {
-    const post = await postRepository.findBySlugPublic(slug).catch(() => undefined)
+export const findPublicPostBySlugCached = cache(
+    (slug: string) => {
+        return unstable_cache(
+            async (slug: string) => {
+                const post = await postRepository.findBySlugPublic(slug).catch(() => undefined)
 
-    if (!post) notFound()
+                if (!post) notFound()
 
-    return post
-}),
-    ['posts'],
-    {
-        tags: [`post-${slug}`],
-    })(slug)
+                return post
+            },
+            [`post-${slug}`],
+            { tags: [`post-${slug}`] }
+        )(slug)
+    }
+)
--- src/repositories/post/drizzle-post-repository.ts
+++ src/repositories/post/drizzle-post-repository.ts
@@ -3,9 +3,12 @@ import { PostRepository } from "./post-repository";
 import { postsTable } from "@/db/drizzle/schemas";
 import { desc } from "drizzle-orm";
 import { PostModel } from "@/models/post/post-model";
+import { logColor } from "@/utils/log-color";
 
 export class DrizzlePostRepository implements PostRepository {
     async findAllPublic(): Promise<PostModel[]> {
+        logColor("findAllPublic", Date.now())
+
         const posts = await drizzleDb.query.posts.findMany({
             orderBy: desc(postsTable.createdAt),
             where: (posts, { eq }) => eq(posts.published, true)
@@ -15,6 +18,8 @@ export class DrizzlePostRepository implements PostRepository {
     }
 
     async findBySlugPublic(slug: string): Promise<PostModel> {
+        logColor("findBySlugPublic", Date.now())
+
         const post = await drizzleDb.query.posts.findFirst({
             where: (posts, { eq, and }) => and(eq(posts.published, true), eq(posts.slug, slug))
         });
@@ -25,15 +30,19 @@ export class DrizzlePostRepository implements PostRepository {
     }
 
     async findAll(): Promise<PostModel[]> {
-         const posts = await drizzleDb.query.posts.findMany({
+        logColor("findAll", Date.now())
+
+        const posts = await drizzleDb.query.posts.findMany({
             orderBy: desc(postsTable.createdAt),
         })
 
         return posts
-     }
+    }
 
-    async findById(id: string): Promise<PostModel> { 
-          const post = await drizzleDb.query.posts.findFirst({
+    async findById(id: string): Promise<PostModel> {
+        logColor("findById", Date.now())
+
+        const post = await drizzleDb.query.posts.findFirst({
             where: (posts, { eq }) => eq(posts.id, id),
         });
 
