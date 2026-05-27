7862cb6cce6d3b43b509999908f68ec7f509eeab
Marcos Reis <marcos.santos@estadao.com>
2026-05-27 00:10:17 -0300

    aula 167

---
+++ src/components/admin/DeletePostButton/index.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import clsx from "clsx";
+import { Trash2Icon } from "lucide-react";
+
+type DeletePostButtonProps = {
+    id: string;
+    title: string;
+}
+
+export function DeletePostButton({ id, title }: DeletePostButtonProps) {
+    return (
+        <button className={clsx(
+            'text-red-500 cursor-pointer transition',
+            '[&_svg]:w-4 [&_svg]:h-4',
+            'hover:scale-120 hover:text-red-700'
+        )}
+            aria-label={`Apagar o post: ${title}`}
+            title={`Apagar o post: ${title}`}
+        >
+            <Trash2Icon />
+        </button>
+    )
+}
