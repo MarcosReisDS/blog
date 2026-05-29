4be328645f2405f8b6fed1a4a52fb2fef60ab65e
Marcos Reis <marcos.santos@estadao.com>
2026-05-28 21:56:14 -0300

    aula 170

--- src/components/admin/DeletePostButton/index.tsx
+++ src/components/admin/DeletePostButton/index.tsx
@@ -1,9 +1,10 @@
 'use client';
 
 import { deletePostAction } from "@/actions/post/delete-post-action";
+import { Dialog } from "@/components/Dialog";
 import clsx from "clsx";
 import { Trash2Icon } from "lucide-react";
-import { useTransition } from "react";
+import { useState, useTransition } from "react";
 
 type DeletePostButtonProps = {
     id: string;
@@ -12,27 +13,46 @@ type DeletePostButtonProps = {
 
 export function DeletePostButton({ id, title }: DeletePostButtonProps) {
     const [isPending, startTransition] = useTransition()
+    const [showDialog, setShowDialog] = useState(false)
 
     function handleClick() {
+        setShowDialog(true)
+    }
+
+    function handleConfirm() {
         startTransition(async () => {
             const result = await deletePostAction(id)
             alert(`O result é: ${result}`)
+            setShowDialog(false)
         })
     }
 
     return (
-        <button className={clsx(
-            'text-red-500 cursor-pointer transition',
-            '[&_svg]:w-4 [&_svg]:h-4',
-            'hover:scale-120 hover:text-red-700',
-            'disabled:text-slate-600 disabled:cursor-not-allowed'
-        )}
-            aria-label={`Apagar o post: ${title}`}
-            title={`Apagar o post: ${title}`}
-            disabled={isPending}
-            onClick={handleClick}
-        >
-            <Trash2Icon />
-        </button>
+        <>
+            <button className={clsx(
+                'text-red-500 cursor-pointer transition',
+                '[&_svg]:w-4 [&_svg]:h-4',
+                'hover:scale-120 hover:text-red-700',
+                'disabled:text-slate-600 disabled:cursor-not-allowed'
+            )}
+                aria-label={`Apagar o post: ${title}`}
+                title={`Apagar o post: ${title}`}
+                disabled={isPending}
+                onClick={handleClick}
+            >
+                <Trash2Icon />
+            </button>
+
+            {showDialog && (
+                <Dialog
+                    isVisible={showDialog}
+                    title="Apagar post?"
+                    content={`Tem certeza de que deseja apagar o post: ${title}`}
+                    onCancel={() => setShowDialog(false)}
+                    onConfirm={handleConfirm}
+                    disabled={isPending}
+                />
+            )}
+        </>
     )
 }
