d67a8344fb5f6e26083447bf176d60e8312633ae
Marcos Reis <marcos.santos@estadao.com>
2026-05-30 22:51:29 -0300

    aula 174

---
+++ src/components/admin/MenuAdmin/index.tsx
@@ -0,0 +1,33 @@
+import clsx from "clsx";
+import { FileTextIcon, HouseIcon } from "lucide-react";
+import Link from "next/link";
+
+export function MenuAdmin() {
+    const navClasses = clsx(
+        'bg-slate-900 text-slate-100 rounded-lg',
+        'flex flex-col',
+        'sm:flex-row sm:flex-wrap',
+        // 'overflow-hidden'
+    )
+    const linkClasses = clsx(
+        '[&>svg]:w-[16px] [&>svg]:h-[16px] px-4',
+        'flex items-center justify-start gap-2',
+        'transition hover:bg-slate-800 rounded-lg',
+        'h-10',
+        'shrink-0'
+    )
+
+    return (
+        <nav className={navClasses}>
+            <a className={linkClasses} href="/" target="_blank">
+                <HouseIcon />
+                Home
+            </a>
+
+            <Link className={linkClasses} href="/admin/post">
+                <FileTextIcon />
+                Post
+            </Link>
+        </nav>
+    )
+}
