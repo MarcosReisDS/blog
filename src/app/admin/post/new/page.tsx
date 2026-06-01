1cad2c7506160b6918acf4b8839484cecc5d8a16
Marcos Reis <marcos.santos@estadao.com>
2026-05-31 22:57:35 -0300

    aula 177

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,9 +1,13 @@
+import { Button } from "@/components/Button";
+
 export const dynamic = "force-dynamic";
 
 export default async function AdminPostNewPage() {
     return (
-        <div>
-
+        <div className="py-16 flex gap-4 flex-wrap">
+            <Button>Confirma</Button>
+            <Button>Confirma</Button>
+            <Button>Confirma</Button>
         </div>
     )
 }
