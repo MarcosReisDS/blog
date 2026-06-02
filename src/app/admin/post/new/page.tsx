aa5a2d96d828c38f172990a25eac44bf6287495d
Marcos Reis <marcos.santos@estadao.com>
2026-06-01 22:59:55 -0300

    aula 178

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -4,10 +4,10 @@ export const dynamic = "force-dynamic";
 
 export default async function AdminPostNewPage() {
     return (
-        <div className="py-16 flex gap-4 flex-wrap">
-            <Button>Confirma</Button>
-            <Button>Confirma</Button>
-            <Button>Confirma</Button>
+        <div className="py-16 flex gap-4 flex-wrap items-center">
+            <Button variant="default" size="sm">Confirma</Button>
+            <Button variant="ghost" size="md">Confirma</Button>
+            <Button variant="danger" size="lg">Confirma</Button>
         </div>
     )
 }
