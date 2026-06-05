2a2ee51244ce82ac4eb60706dfd71a8ce29d41c3
Marcos Reis <marcos.santos@estadao.com>
2026-06-04 23:48:26 -0300

    aula 182

--- src/components/admin/ManagePostForm/index.tsx
+++ src/components/admin/ManagePostForm/index.tsx
@@ -3,8 +3,12 @@
 import { Button } from "@/components/Button";
 import { InputCheckbox } from "@/components/InputCheckbox";
 import { InputText } from "@/components/InputText";
+import { MarkdownEditor } from "@/components/MarkdownEditor";
+import { useState } from "react";
 
 export function ManagePostForm() {
+    const [contentValue, setContentValue] = useState('');
+
     return (
         <form action='' className="mb-16 mt-10">
             <div className="flex flex-col gap-6">
@@ -12,6 +16,14 @@ export function ManagePostForm() {
                 <InputText labelText="Nome" placeholder="Digite seu nome" />
 
                 <InputCheckbox labelText="Aceito os termos de uso" />
+
+                <MarkdownEditor
+                    labelText="Conteúdo"
+                    disabled={false}
+                    textAreaName="content"
+                    value={contentValue}
+                    setValue={setContentValue}
+                />
 
                 <div className="mt-4">
                     <Button type="submit">Enviar</Button>
