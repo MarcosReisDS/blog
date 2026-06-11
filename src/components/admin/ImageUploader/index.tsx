e7cde2f29ff16c1fddb409b379e25707ff5f339b
Marcos Reis <marcos.santos@estadao.com>
2026-06-10 21:56:27 -0300

    aula 185

--- src/components/admin/ImageUploader/index.tsx
+++ src/components/admin/ImageUploader/index.tsx
@@ -1,13 +1,15 @@
 'use client';
 
+import { uploadImageAction } from "@/actions/upload/upload-image-action";
 import { Button } from "@/components/Button";
 import { IMAGE_UPLOADER_MAX_SIZE } from "@/lib/post/constants";
 import { ImageUpIcon } from "lucide-react";
-import { useRef } from "react";
+import { useRef, useTransition } from "react";
 import { toast } from "react-toastify";
 
 export function ImageUploader() {
     const inputFileRef = useRef<HTMLInputElement>(null)
+    const [isUploading, startTransition] = useTransition()
 
     function handleChooseFile() {
         if (!inputFileRef.current) return;
@@ -33,6 +35,10 @@ export function ImageUploader() {
 
         const formData = new FormData()
         formData.append('file', file)
+
+        startTransition(async () => {
+            const result = await uploadImageAction();
+        })
 
         fileInput.value = '';
     }
