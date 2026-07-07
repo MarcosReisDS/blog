c906ba650f428947e65074818312417c0e973514
Marcos Reis <marcos.santos@estadao.com>
2026-07-07 01:16:52 -0300

    aula 204

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,9 +1,8 @@
+import { LoginForm } from "@/components/admin/LoginForm";
+
 export const dynamic = "force-dynamic";
 
 export default async function AdminLoginPage() {
-    return (
-        <div>
 
-        </div>
-    )
+    return <LoginForm />
 }
