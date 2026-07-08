b8628b8dae368501e5492df6529dc1b1b40e7ffd
Marcos Reis <marcos.santos@estadao.com>
2026-07-07 23:35:25 -0300

    aula 206

--- src/actions/login/login-action.ts
+++ src/actions/login/login-action.ts
@@ -1,5 +1,6 @@
 'use server';
 
+import { verifyPassword } from "@/lib/login/manage-login";
 import { asyncDelay } from "@/utils/async-delay";
 
 type LoginActionState = {
@@ -17,13 +18,28 @@ export async function loginAction(state: LoginActionState, formData: FormData) {
         }
     }
 
-    const username = formData.get('username')?.toString() || '';
-    const password = formData.get('password')?.toString() || '';
+    const username = formData.get('username')?.toString().trim() || '';
+    const password = formData.get('password')?.toString().trim() || '';
+
+    if (!username || !password) {
+        return {
+            username,
+            error: 'Digite o usuário e a senha'
+        }
+    }
 
     const isUsernameValid = username === process.env.LOGIN_USER;
+    const isPasswordValid = await verifyPassword(password, process.env.LOGIN_PASS || '')
+
+    if (!isUsernameValid || !isPasswordValid) {
+        return {
+            username,
+            error: 'Usuário ou senha inválidos'
+        }
+    }
 
     return {
-        username: '',
-        error: ''
+        username,
+        error: 'Usuário logado com sucesso!'
     }
 }
