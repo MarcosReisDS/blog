b8971e954a1842157d157b27cc6231f5c88f4ac8
Marcos Reis <marcos.santos@estadao.com>
2026-07-09 13:10:20 -0300

    aula 211

--- src/components/admin/MenuAdmin/index.tsx
+++ src/components/admin/MenuAdmin/index.tsx
@@ -1,14 +1,16 @@
 'use client'
 
+import { logoutAction } from "@/actions/login/logout-action";
 import clsx from "clsx";
-import { CircleXIcon, FileTextIcon, HouseIcon, MenuIcon, PlusIcon } from "lucide-react";
+import { CircleXIcon, FileTextIcon, HourglassIcon, HouseIcon, LogOutIcon, MenuIcon, PlusIcon } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useTransition } from "react";
 
 export function MenuAdmin() {
     const [isOpen, setIsOpen] = useState(false)
     const pathname = usePathname()
+    const [isPending, startTrasition] = useTransition()
 
     useEffect(() => {
         setIsOpen(false)
@@ -34,6 +36,13 @@ export function MenuAdmin() {
         'text-blue-200 italic',
         'sm:hidden'
     )
+
+    function handleLogout(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+        e.preventDefault();
+        startTrasition(async () => {
+            await logoutAction()
+        });
+    }
 
     return (
         <nav className={navClasses}>
@@ -68,6 +77,22 @@ export function MenuAdmin() {
                 <PlusIcon />
                 Criar Post
             </Link>
+
+            <a onClick={handleLogout} href="#" className={linkClasses}>
+                {isPending && (
+                    <>
+                        <HourglassIcon />
+                        Aguarde...
+                    </>
+                )}
+                
+                {!isPending && (
+                    <>
+                        <LogOutIcon />
+                        Sair
+                    </>
+                )}
+            </a>
         </nav>
     )
 }
